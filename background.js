18f8d6359bc7bc601428fc5c75e8630ae5f2b762
o89p <onveshasho+github@gmail.com>
2017-12-14 00:48:25 +0900

    Implement auto parrot

--- background.js
+++ background.js
@@ -1,20 +1,48 @@
+let parrotTitle = null;
+
 function toggleParrot(title) {
   if(title == "Preparrot") {
     browser.browserAction.setTitle({title: "Partyparrot"});
     browser.browserAction.setIcon({path: "icons/partyparrot.gif"});
     browser.tabs.executeScript({file: "parrotShift.js"});
-  } else {
+  } else if(title == "Partyparrot") {
     browser.browserAction.setTitle({title: "Preparrot"});
     browser.browserAction.setIcon({path: "icons/preparrot.gif"});
   }
+  var title = browser.browserAction.getTitle({});
+  title.then((e) => {
+    parrotTitle = e;
+    console.log(parrotTitle);
+  });
 }
 
-browser.browserAction.onClicked.addListener(() => {
-  var gettingTitle = browser.browserAction.getTitle({});
-  gettingTitle.then(toggleParrot);
-})
+function setParrot() {
+  if(parrotTitle == "Partyparrot") {
+    browser.tabs.executeScript({file: "parrotShift.js"});    
+  }
+}
 
-var portFromCS;
+
+
+// listen to parrot button clicking
+browser.browserAction.onClicked.addListener(() => {
+  let title = browser.browserAction.getTitle({});
+  title.then(toggleParrot);
+});
+
+// listen to tab URL changes
+browser.tabs.onUpdated.addListener(setParrot);
+
+// listen to tab switching
+browser.tabs.onActivated.addListener(setParrot);
+
+// listen for window switching
+browser.windows.onFocusChanged.addListener(setParrot);
+
+
+
+let portFromCS;
+
 function connected(p) {
   portFromCS = p;
   portFromCS.postMessage({
@@ -23,6 +51,3 @@ function connected(p) {
 }
 
 browser.runtime.onConnect.addListener(connected);
-/*
-bookmark-itを参考にタブ変更時のリスナーを設定し，各々のタブでparrotShift()を実行すればよい？
-*/
